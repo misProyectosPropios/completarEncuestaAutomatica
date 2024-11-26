da284f3fd83db88bdd247051eebd91ae82f4ca51
misProyectosPropios <inakifrutos@outlook.com>
2024-11-26 22:08:17 +0100

    Update noRespondeSeleccionado.js

--- noRespondeSeleccionado.js
+++ noRespondeSeleccionado.js
@@ -1,1 +1,23 @@
 
+
+ListOfIframe = document.getElementsByTagName("iframe");
+
+console.log(ListOfIframe);
+
+for(const iframe of ListOfIframe) {
+  h2Tags = iframe.contentDocument.getElementsByTagName("h2");
+  console.log(h2Tags);
+  for (const title of h2Tags) {
+    if (title.firstChild.textContent.includes("Evaluación") && confirm("Autocompleta campo?")) {
+      //Seleccionar campos correctos (No responde)
+    	radioButtons = title.parentElement.getElementsByTagName("input");
+      for (const button of radioButtons) {
+      	if (button.type === 'radio' && button.parentElement.childNodes[2].data.includes("No Responde")) {
+      		button.checked = true;
+      	}
+    	}
+    }
+  }
+}
+
+
