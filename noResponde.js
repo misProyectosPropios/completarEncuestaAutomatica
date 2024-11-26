6b39fd9ada5914338915e50399762c7d17b930a8
misProyectosPropios <inakifrutos@outlook.com>
2024-11-26 21:17:12 +0100

    Update noResponde.js

--- noResponde.js
+++ noResponde.js
@@ -1,1 +1,6 @@
-
+radioButtons = document.getElementsByTagName("input");
+for (const button of radioButtons) {
+  	if (button.type === 'radio' && button.parentElement.childNodes[2].data.includes("No Responde")) {
+      button.checked = true;
+    }
+}
